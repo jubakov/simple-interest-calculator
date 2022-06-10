879687260bd8072faecefb5fd410a9a34073e6b2
ADJOUT Jugurta <juguraadjout@gmail.com>
2022-06-10 17:37:44 +0200

    my first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+unction compute()
+{
+    var pricipal= document.getElementById("pricipal").value;
+    var rate= document.getElementById("rate").value;
+    var years= document.getElementById("years").value;
+    var interest=principal*years*rate/100
+    var newdate().getFullyear()+parseInt(years);
+  p = document.getElementById("principal").value;
+    
+}
+function updateRate()
+{
+    var rateval= document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}     
